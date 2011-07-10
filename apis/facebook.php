6857a5a38573d4220ff0d5f4983599f485640465
Shillo Ben-David <sshilo@gmail.com>
2011-07-10 03:57:10 +0300

    Made FacebookApp a singleton-like class

--- apis/facebook.php
+++ apis/facebook.php
@@ -15,8 +15,18 @@ class FacebookApp
 	private $id;
 	private $secretKey;
 	
+	/**
+	 * Loads FacebookApp instance using application identifier and secret key from cache.
+	 *
+	 * @return void
+	 * @author Shillo Ben-David
+	 **/
+	public static function loadFromCache ( $id, $secretKey )
+	{
+		
+	}
 	
-	public function __construct($id, $secretKey)
+	private function __construct($id, $secretKey)
 	{
 		$this->id 			= $id;
 		$this->secretKey 	= $secretKey;
